c0b507ce9b343fce8a2ba65fda58ba2ee34be335
MohammadHosein Paymard <mhpaymard010@gmail.com>
2022-08-24 09:23:47 +0430

    add some features to developer routes

--- app/routes/developer.route.js
+++ app/routes/developer.route.js
@@ -10,15 +10,15 @@ const bcrypt = require('bcrypt');
 
 /**
  * @swagger
- *  /developer/password-hash/{password}
+ *  /developer/password-hash/{password}:
  *      get:
  *          tags: [Developer-Routes]
  *          summary: hash data with bcrypt
  *          parameters:
- *          -   in: path
- *              type: string
- *              name: password
- *              required: true
+ *              -   in: path
+ *                  type: string
+ *                  name: password
+ *                  required: true
  *          responses:
  *              200:
  *                  description: success
@@ -31,6 +31,32 @@ router.get("/password-hash/:password",(req,res,next)=>{
         status:200,
         success:true,
         hashedPassword
+    });
+})
+
+/**
+ * @swagger
+ *  /developer/getRandomNumber/{digit}:
+ *      get:
+ *          tags: [Developer-Routes]
+ *          summary: random number in any digit
+ *          parameters:
+ *              -   in: path
+ *                  type: string
+ *                  name: digit
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: success
+ */
+ router.get("/getRandomNumber/:digit",(req,res,next)=>{
+    const {digit} = req.params;
+    const digitInt = parseInt(digit) || 1;
+    const randomNumber = Math.floor(Math.random() * (Math.pow(10,digitInt)*9/10)) + Math.pow(10,digitInt-1);
+    return res.status(200).json({
+        status:200,
+        success:true,
+        digit:randomNumber
     });
 })
 
